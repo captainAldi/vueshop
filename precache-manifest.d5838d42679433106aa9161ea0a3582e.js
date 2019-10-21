c4fdc26b639884c174e2be4760198e4314663909
captainAldi <renaldiyulvianda@yahoo.come>
2019-10-21 17:29:09 +0700

    Initial dist subtree commit

---
+++ precache-manifest.d5838d42679433106aa9161ea0a3582e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "44a5707f8e72df83a8d8",
+    "url": "/vueshop/js/chunk-0025aa1c.37a7bac1.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueshop/robots.txt"
+  },
+  {
+    "revision": "1e6945b9e31f3802d815",
+    "url": "/vueshop/css/chunk-vendors.57ee95a9.css"
+  },
+  {
+    "revision": "5788d7878336673ae4b9",
+    "url": "/vueshop/js/alert.c57a1bb1.js"
+  },
+  {
+    "revision": "f9104e74f921acdc1538",
+    "url": "/vueshop/js/search.0777dc5f.js"
+  },
+  {
+    "revision": "8ff51557ed9f71629e8f",
+    "url": "/vueshop/js/book.f343b6fd.js"
+  },
+  {
+    "revision": "6ab7c388cc836319ca15",
+    "url": "/vueshop/js/book-item.dc58a7ea.js"
+  },
+  {
+    "revision": "1c543d5315ca45d5ddcf",
+    "url": "/vueshop/js/register.c915fd4a.js"
+  },
+  {
+    "revision": "24b3b64dba5bb47da7dd",
+    "url": "/vueshop/js/cart.cd912315.js"
+  },
+  {
+    "revision": "da63671ccdef9f456172",
+    "url": "/vueshop/js/profile.c4b04eac.js"
+  },
+  {
+    "revision": "9f023eb5d481e72efe6a",
+    "url": "/vueshop/js/categories.fb2d1d66.js"
+  },
+  {
+    "revision": "a9076d1926faba4bd9fa",
+    "url": "/vueshop/js/payment.77fdc209.js"
+  },
+  {
+    "revision": "e4b4fb482abd59e1d030",
+    "url": "/vueshop/js/category.5c8696f6.js"
+  },
+  {
+    "revision": "b0653b441e6b1258ef9b",
+    "url": "/vueshop/js/my-order.0e2853e9.js"
+  },
+  {
+    "revision": "742393da5615afc4b3d9",
+    "url": "/vueshop/js/checkout.b08d5487.js"
+  },
+  {
+    "revision": "e2a8e8ec936ca4bc7abf",
+    "url": "/vueshop/js/login.d7c1c6e3.js"
+  },
+  {
+    "revision": "1e6945b9e31f3802d815",
+    "url": "/vueshop/js/chunk-vendors.24b3976e.js"
+  },
+  {
+    "revision": "db5e938f44cc71da665b",
+    "url": "/vueshop/js/app.5f0a7c2f.js"
+  },
+  {
+    "revision": "9f023eb5d481e72efe6a",
+    "url": "/vueshop/css/categories.d9e045ca.css"
+  },
+  {
+    "revision": "e69d55d589659099104e",
+    "url": "/vueshop/js/about.e38a2596.js"
+  },
+  {
+    "revision": "1c543d5315ca45d5ddcf",
+    "url": "/vueshop/css/register.35b17141.css"
+  },
+  {
+    "revision": "b0653b441e6b1258ef9b",
+    "url": "/vueshop/css/my-order.c57ee95b.css"
+  },
+  {
+    "revision": "a9076d1926faba4bd9fa",
+    "url": "/vueshop/css/payment.c57ee95b.css"
+  },
+  {
+    "revision": "607596bfa3a1fe0c4d654b6832bf59b0",
+    "url": "/vueshop/index.html"
+  },
+  {
+    "revision": "da63671ccdef9f456172",
+    "url": "/vueshop/css/profile.c57ee95b.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/vueshop/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "f9104e74f921acdc1538",
+    "url": "/vueshop/css/search.c02abeab.css"
+  },
+  {
+    "revision": "44a5707f8e72df83a8d8",
+    "url": "/vueshop/css/chunk-0025aa1c.d9e045ca.css"
+  },
+  {
+    "revision": "742393da5615afc4b3d9",
+    "url": "/vueshop/css/checkout.95c9d861.css"
+  },
+  {
+    "revision": "e4b4fb482abd59e1d030",
+    "url": "/vueshop/css/category.d9e045ca.css"
+  },
+  {
+    "revision": "24b3b64dba5bb47da7dd",
+    "url": "/vueshop/css/cart.85aadb5e.css"
+  },
+  {
+    "revision": "8ff51557ed9f71629e8f",
+    "url": "/vueshop/css/book.04348105.css"
+  },
+  {
+    "revision": "5788d7878336673ae4b9",
+    "url": "/vueshop/css/alert.3e507f7b.css"
+  }
+];
